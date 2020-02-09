b3c3d4b2efe9d512c58382a8016d7d961a0713cd
Abishak Kodi <abishak.kodi@gmail.com>
2020-02-08 16:54:28 -0800

    got sockets working

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io')(server);
+const io = require('socket.io')(server);
 const chalk = require('chalk');
 const log = console.log;
-
 
 const port = process.env.PORT || 3000;
 
@@ -15,6 +14,12 @@ app.get('/', (req, res)=> {
     res.status(200).send('<h1> We gucci </h1>');
 });
 
+io.on('connection', (socket)=> {
+    socket.on('chat-message', (msg)=> {
+        log(chalk.blue(msg));
+        io.emit('chat-message', msg)
+    });
+});
 
 server.listen(port, () => {
     log(chalk.green('listening on port: ' + port));
